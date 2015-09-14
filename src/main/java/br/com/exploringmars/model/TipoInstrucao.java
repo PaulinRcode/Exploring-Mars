c36efe0d8ed37145b5913007f09bdd73b4100a41
Paulinho <paulinhosiri@hotmail.com>
2015-09-13 22:39:16 -0300

    Definição das instruções esquerda, direita e frente para controlar a sonda.

--- src/main/java/br/com/exploringmars/model/TipoInstrucao.java
+++ src/main/java/br/com/exploringmars/model/TipoInstrucao.java
@@ -5,7 +5,14 @@ package br.com.exploringmars.model;
  * @author Paulinho
  */
 public enum TipoInstrucao {
- 
+
     L("Esquerda"),
-    
+    R("Direita"),
+    M("Frente");
+
+    private String instrucao;
+
+    TipoInstrucao(String instrucao) {
+        instrucao = instrucao;
+    }
 }
