28399ea6b239334f8eca3edd740e89e35ed0f143
Paulinho <paulinhosiri@hotmail.com>
2015-09-13 21:57:51 -0300

    Implementação inicial dos métodos posicionarN1 e posicionarN2 para colocar as naves na posição inicial.

--- src/main/java/br/com/exploringmars/bo/SondaBO.java
+++ src/main/java/br/com/exploringmars/bo/SondaBO.java
@@ -1,5 +1,48 @@
 package br.com.exploringmars.bo;
 
-public class SondaBo {
+import br.com.exploringmars.model.SondaDTO;
+import br.com.exploringmars.model.TipoInstrucao;
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.JTextField;
+
+public class SondaBO {
+
+    private int abscissa, ordenada;
+    private TipoInstrucao tipoInstrucao;
+
+    public void posicionarN1(int x, int y, String direcaoCardinal) {
+
+        x += abscissa;
+        y += ordenada;
+        // TODO implementar a direção da nave conforme a rosa dos ventos
+
+    }
+
+    public void posicionarN2(int x, int y, String direcaoCardinal) {
+
+        x += abscissa;
+        y += ordenada;
+        // TODO implementar a direção da nave conforme a rosa dos ventos
+
+    }
+
+    public void explorar(TipoInstrucao tipoInstrucao) {
+
+    /*    switch (tipoInstrucao)
+        {
+            case L:
+                posicaoAtual;
+                break;
+            case R:
+                posicaoAtual;
+                break;
+            case M:
+                posicaoAtual;
+                break;
+        }
+    */        
+    }
+    
 
 }
