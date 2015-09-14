f7c0668dc7f2104552e850bf2c9a2f680d510e36
Paulinho <paulinhosiri@hotmail.com>
2015-09-13 22:37:26 -0300

    Definição em constante das direções cardinais da rosa dos ventos em inglês.

--- src/main/java/br/com/exploringmars/model/TipoDirecaoCardinal.java
+++ src/main/java/br/com/exploringmars/model/TipoDirecaoCardinal.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.exploringmars.model;
 
 /**
@@ -10,5 +5,15 @@ package br.com.exploringmars.model;
  * @author Paulinho
  */
 public enum TipoDirecaoCardinal {
-    
+
+    N("North"),
+    W("West"),
+    E("East"),
+    S("South");
+
+    private String direcaoCardinal;
+
+    private TipoDirecaoCardinal(String direcaoCardinal) {
+        direcaoCardinal = direcaoCardinal;
+    }
 }
